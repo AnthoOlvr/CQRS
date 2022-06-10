e68607d6b709a339d0c87793cb96d3dae89352f9
Anthony De Oliveira <anthony_deoliveira@outlook.fr>
2022-06-10 14:33:31 +0200

    Start EventSourcing

--- Cqrs.implementation.Stock/Event.cs
+++ Cqrs.implementation.Stock/Event.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace Cqrs.implementation.Stock
 {
     public sealed class Event
     {
         public int id { get; set; }
-        public int ProductStockId { get; set; }
+        public Guid DataSourceGuid { get; set; }
+        public DateTime RecordingDate { get; set; }
+        public JsonDocument Datas { get; set; }
     }
 }
